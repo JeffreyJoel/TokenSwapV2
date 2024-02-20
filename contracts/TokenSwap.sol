be69ee9e178ff501d43e7c8d849b0d7cac5fd308
JeffreyJoel <jowoloko@gmail.com>
2024-02-20 13:30:33 +0100

    begin token swap

---
+++ contracts/TokenSwap.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract TokenSwap {
+    address tokenA;
+    address tokenB;
+    address owner;
+
+    constructor() {}
+
+    function swapAToB () external {}
+    function swapBToA () external {}
+}
