67154bb05ed08718ab322ac98a90cd2d9582b7f9
JeffreyJoel <jowoloko@gmail.com>
2024-02-21 11:54:33 +0100

    update swap contract

--- contracts/TokenSwap.sol
+++ contracts/TokenSwap.sol
@@ -3,12 +3,27 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TokenSwap {
-    address tokenA;
-    address tokenB;
-    address owner;
+    IERC20 tokenA;
+    IERC20 tokenB;
+    uint256 public exchangeRate;
 
-    constructor() {}
+    constructor(address _tokenA, address _tokenB, uint256 _exchangeRate) {
+        tokenA = IERC20(_tokenA);
+        tokenB = IERC20(_tokenB);
+        exchangeRate = _exchangeRate;
+    }
 
-    function swapAToB () external {}
-    function swapBToA () external {}
+    function swapAToB(uint256 _amount) external {
+        require(tokenA.balanceOf(msg.sender) >= _amount, "Not enough A tokens");
+     
+        tokenA.transferFrom(msg.sender, address(this), _amount);
+        tokenB.transfer(msg.sender, _amount * exchangeRate);
+    }
+
+    function swapBToA(uint256 _amount) external {
+        require(tokenB.balanceOf(msg.sender) >= _amount, "Not enough B tokens");
+
+        tokenB.transferFrom(msg.sender, address(this), _amount);
+        tokenA.transfer(msg.sender, _amount / exchangeRate);
+    }
 }
